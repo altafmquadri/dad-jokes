e62d398cfd681561d1ea128989c3f10c319ddfc4
Altaf Quadri <altafmquadri@gmail.com>
2020-02-16 15:32:35 -0500

    smiley css, and scores css

--- src/Joke.js
+++ src/Joke.js
@@ -2,6 +2,39 @@ import React from 'react';
 import './Joke.css'
 
 const Joke = (props) => {
+    const getColor = (score) => {
+        if (score >= 15) {
+            return '#4CAF50';
+        } else if (score >= 12) {
+            return "#8BC34A"
+        } else if (score >= 9) {
+            return "#CDDC39"
+        } else if (score >= 6) {
+            return "#FFEB3B"
+        } else if (score >= 3) {
+            return "#FFC107"
+        } else if (score >= 0) {
+            return "#FF9800"
+        }
+    }
+
+    const getEmoji = (score) => {
+        if (score >= 15) {
+            return 'em em-rolling_on_the_floor_laughing'
+        } else if (score >= 12) {
+            return "em em-laughing"
+        } else if (score >= 9) {
+            return "em em-smiley"
+        } else if (score >= 6) {
+            return "em em-slightly_smiling_face"
+        } else if (score >= 3) {
+            return "em em-neutral_face"
+        } else if (score >= 0) {
+            return "em em-confused"
+        } else {
+            return 'em em-angry'
+        }
+    }
     return (
         props.jokes.map(j => (
             <div key={j.id}
@@ -10,15 +43,16 @@ const Joke = (props) => {
                     <i
                         onClick={() => props.increaseScore(j.id)}
                         className="fas fa-arrow-up"></i>
-                    <span className="Joke-votes">{j.score}</span>
+                    <span style={{ borderColor: getColor(j.score) }}
+                        className="Joke-votes">{j.score}</span>
                     <i
                         onClick={() => props.decreaseScore(j.id)}
                         className="fas fa-arrow-down"></i>
                 </div>
                 <div className="Joke-text">{j.joke}</div>
                 <div className="Joke-smiley">
-                    <i class="em em-rolling_on_the_floor_laughing"
-                        aria-label="ROLLING ON THE FLOOR LAUGHING">
+                    <i class={getEmoji(j.score)}
+                        aria-label={getEmoji(j.score)}>
                     </i>
                 </div>
             </div>
